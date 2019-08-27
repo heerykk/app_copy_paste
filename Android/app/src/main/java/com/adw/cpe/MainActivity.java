5adb38474cc411ed9ec8b5ab21f438261c917c0b
heerykk <henry.k.adarkwa@gmail.com>
2019-08-27 14:06:31 +0100

    added barcode reader test

--- Android/app/src/main/java/com/adw/cpe/MainActivity.java
+++ Android/app/src/main/java/com/adw/cpe/MainActivity.java
@@ -1,14 +1,66 @@
 package com.adw.cpe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 public class MainActivity extends AppCompatActivity {
+    EditText ed1, ed2;
+    Button b1, b2;
+
+    private ClipboardManager myClipboard;
+    private ClipData myClip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed2 = (EditText) findViewById(R.id.editText2);
+        ed1 = (EditText) findViewById(R.id.editText);
+
+        b2 = (Button) findViewById(R.id.button2);
+        b1 = (Button) findViewById(R.id.button);
+
+
+        myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String text;
+                text = ed1.getText().toString();
+
+                myClip = ClipData.newPlainText("text", text);
+                myClipboard.setPrimaryClip(myClip);
+
+                Toast.makeText(getApplicationContext(), "Text Copied",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ClipData abc = myClipboard.getPrimaryClip();
+                ClipData.Item item = abc.getItemAt(0);
+
+                String text = item.getText().toString();
+                ed2.setText(text);
+
+                Toast.makeText(getApplicationContext(), "Text Pasted",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
